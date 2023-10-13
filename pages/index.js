b7ceba8b1e26fbfbebe5fbde83c8bc12b3420ecb
Felipe Londoño <felipelo298@gmail.com>
2023-10-12 20:17:46 -0500

    add message to home page

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
+      <div className={styles.homeText}>
+        <h1 className={styles.title}>Welcome to the Rick and Morty App!</h1>
+        <h3 className={styles.homeDescription}>
+          Welcome to our exciting Rick and Morty application. Dive into the
+          multiverse and explore the fascinating world of Rick, Morty, and their
+          friends through our user-friendly interface. Discover intriguing
+          characters, adventure-filled episodes, and out-of-this-world
+          locations.
+        </h3>
+        <h3>With our app, you can:</h3>
+        <ul>
+          <li>
+            <span className={styles.textStrong}>Explore Characters:</span>
+            Get to know your favorite characters and discover interesting
+            details about them.
+          </li>
+          <li>
+            <span className={styles.textStrong}>Discover Episodes:</span>
+            Immerse yourself in Rick and Morty's interdimensional escapades
+            through a wide selection of episodes.
+          </li>
+          <li>
+            <span className={styles.textStrong}>Investigate Locations:</span>
+            Investigate Locations: Explore the weirdest and most wonderful
+            places the multiverse has to offer.
+          </li>
+        </ul>
+
+        <p>
+          Have fun exploring, and enjoy a laughter-filled, fun-packed experience
+          in our Rick and Morty universe!
+        </p>
+      </div>
+
       <div className={styles.cardsContainer}>
         <MainCard
           image={characterImage}
